6c7d8ab17eb02c6c1faf542058f4ddab0e236617
Tponomareva2 <133492721+Tponomareva2@users.noreply.github.com>
2023-05-13 13:31:24 +0300

    Update pingpong.py
    добавила функции

--- pingpong.py
+++ pingpong.py
@@ -6,10 +6,40 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 display.set_caption('Шутер')
 background = transform.scale(image.load('galaxy.jpg'), (700, 500))
-game = True
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player1(GameSprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_w] and self.rect.y > 5:
+            self.rect.y += self.speed
+        if keys_pressed[K_s] and self.rect.y < win_height - 80:
+            self.rect.y -= self.speed
+
+class Player2(GameSprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_UP] and self.rect.y > 5:
+            self.rect.x += self.speed
+        if keys_pressed[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.x -= self.speed
+
+
+clock = time.Clock()
+FPS = 60
+game = True
 while game:
     window.blit(background,(0,0))
-    
+    clock.tick(FPS)
  
     display.update()
