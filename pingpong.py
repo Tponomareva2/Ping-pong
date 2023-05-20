0bc94b3b760fbd7ac798e8e094f5076784d98cc5
Tponomareva2 <133492721+Tponomareva2@users.noreply.github.com>
2023-05-20 13:29:54 +0300

    Update pingpong.py
    Добавила мяч

--- pingpong.py
+++ pingpong.py
@@ -35,23 +35,36 @@ class Player(GameSprite):
 
 racket = Player('racket1.png', 100, 400, 5)
 racket2 = Player('racket2.png', 500, 400, 5)
+ball = GameSprite('ping.png', 100, 200, 5)
+speed_x = 3
+speed_y = 3
 
 clock = time.Clock()
 FPS = 60
 game = True
-
+finish = False
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-   
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+    if sprite.collide_rect(racket, ball) or  sprite.collide_rect(racket2, ball):
+        speed_x *= -1
+    
+    if ball.rect.y > win_height-50 or ball.rect.y < 0:
+        speed_y *= -1
+    
     window.blit(background,(0,0))
     clock.tick(FPS)
     racket.update_l()
     racket.reset()
     racket2.update_r()
     racket2.reset()
-    
+    ball.reset()
+   
     
     display.update()
