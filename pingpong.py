f4de8547daaf6ed0e7c95e83542cabcc86d02ff8
Tponomareva2 <133492721+Tponomareva2@users.noreply.github.com>
2023-05-20 12:52:53 +0300

    Update pingpong.py
    Сделала две ракетки.

--- pingpong.py
+++ pingpong.py
@@ -4,7 +4,7 @@ from random import randint
 win_width = 700
 win_height = 500
 window = display.set_mode((win_width, win_height))
-display.set_caption('Шутер')
+display.set_caption('пинг понг')
 background = transform.scale(image.load('galaxy.jpg'), (700, 500))
 
 class GameSprite(sprite.Sprite):
@@ -18,28 +18,40 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player1(GameSprite):
-    def update(self):
+class Player(GameSprite):
+    def update_l(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_w] and self.rect.y > 5:
-            self.rect.y += self.speed
-        if keys_pressed[K_s] and self.rect.y < win_height - 80:
             self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
-class Player2(GameSprite):
-    def update(self):
+    def update_r(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_UP] and self.rect.y > 5:
-            self.rect.x += self.speed
+            self.rect.y -= self.speed
         if keys_pressed[K_DOWN] and self.rect.y < win_height - 80:
-            self.rect.x -= self.speed
+            self.rect.y += self.speed
 
+racket = Player('racket1.png', 100, 400, 5)
+racket2 = Player('racket2.png', 500, 400, 5)
 
 clock = time.Clock()
 FPS = 60
 game = True
+
+
 while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+   
     window.blit(background,(0,0))
     clock.tick(FPS)
- 
+    racket.update_l()
+    racket.reset()
+    racket2.update_r()
+    racket2.reset()
+    
+    
     display.update()
